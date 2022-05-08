a3221a83d9fb8fe317bee85db96d2e2c8ec616e6
Andromeda Kepecs <amkepecs@gmail.com>
2022-05-08 16:33:44 -0700

    Working play music from SD card, force sensor, push button (all separate). Had to reformat SD card to FAT32 and uncomment line in TMRPCM library

--- Talking_Plush/Talking_Plush.ino
+++ Talking_Plush/Talking_Plush.ino
@@ -12,6 +12,7 @@ Library References:
 - https://www.arduino.cc/en/reference/SD
 - https://www.arduino.cc/en/reference/SPI 
 - https://www.arduino.cc/reference/en/libraries/tmrpcm/ 
+  - Note: uncommented "#define DISABLE_SPEAKER2" in pcmConfig.h to play sound correctly
 
 Hookup:
 - From Arduino to CP2102 USB to TTL Module:
@@ -32,6 +33,7 @@ Hookup:
 - Force Sensor
   - One end to 5V
   - Other end to analog in, 10k ohm resistor, then ground
+- Button connected to 10k ohm resistor. Connected to digital pin 2
 
 
 TODO Force sensing and conditional sound playing
@@ -42,14 +44,20 @@ TODO microphone recording, storing to SD and playback
 #include "SPI.h"
 #include "TMRpcm.h"
 
+// Digital
+#define BUTTON_PIN 2
 #define CHIP_SELECT 8
 #define SPEAKER_PIN 9
+
+// Analog
 #define FORCE_SENSOR_PIN 0
+
 #define BAUD_RATE 19200 // View monitor at 9600 baud
 
 const int VOLUME = 6;
 
 Sd2Card card;
+
 TMRpcm tmrpcm;
 
 void setup() {
@@ -58,9 +66,10 @@ void setup() {
 
   Serial.println("Initializing SD card");
 
-  pinMode(10, OUTPUT);
+  pinMode(10, OUTPUT); // SS
+  pinMode(BUTTON_PIN, INPUT);
 
-  if (!card.init(SPI_HALF_SPEED, CHIP_SELECT)) {
+  if (!SD.begin(CHIP_SELECT)) {
     Serial.println("Initialization failed");
     return;
   } 
@@ -68,13 +77,48 @@ void setup() {
     Serial.println("Wiring is correct and a card is present");
   }
 
+  // File my_file = SD.open("1.wav");
+  // if (my_file) {
+  //   while (my_file.available()) {
+  //     Serial.write(my_file.read());
+  //   }
+
+  //   my_file.close();
+  // }
+  // else {
+  //   Serial.println("Error");
+  // }
+
   tmrpcm.setVolume(VOLUME);
-  tmrpcm.play("0000.wav");
+  tmrpcm.play("1.wav");
+  delay(3000);
+  tmrpcm.play("2.wav");
+  delay(3000);
+  tmrpcm.play("3.wav");
+  delay(3000);
+  tmrpcm.play("4.wav");
+  delay(3000);
+  tmrpcm.play("5.wav");
+  delay(3000);
+  tmrpcm.play("6.wav");
+  delay(3000);
   Serial.println(tmrpcm.isPlaying());
+
+  
 }
 
 void loop() { 
   delay(1000);
+  Serial.println();
+
+  int button_state = digitalRead(BUTTON_PIN);
+  if (button_state == HIGH) {
+    Serial.println("Button pressed");
+  }
+  else {
+    Serial.println("Button not pressed");
+  }
+
   int analog_force = analogRead(FORCE_SENSOR_PIN);
   Serial.println(analog_force);
   // TODO play different sounds based on amount of pressure
@@ -84,7 +128,7 @@ void loop() {
   else if (analog_force < 500) {
     Serial.println("React to light touch");
   }
-  else if (analog_force < 800) {
+  else if (analog_force < 900) {
     Serial.println("React to stronger touch");
   }
   else {
